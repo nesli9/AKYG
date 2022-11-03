9e07082ccb6195b493dd7c5a9c58ac27fcb63e42
nesli9 <doanneslihan@gmail.com>
2022-11-03 23:12:40 +0300

    REST API servisi oluşturma

--- restApiServisi/restApiServis.py
+++ restApiServisi/restApiServis.py
@@ -5,7 +5,7 @@ from flask_restful import Resource, Api, reqparse
 app = Flask(__name__)
 api = Api(app)
 
-OGRENCİLER = {
+OGRENCILER = {
     '1': {'ad': 'Neslihan', 'yas': 23, 'bolum': 'Mühendislik'},
     '2': {'ad': 'Tuğçe', 'yas': 20, 'bolum': 'Mühendislik'},
     '3': {'ad': 'Cemile', 'yas': 21, 'bolum': 'Mühendislik'},
@@ -16,47 +16,47 @@ parser = reqparse.RequestParser()
 
 class StudentsList(Resource):
     def get(self):
-        return OGRENCİLER
+        return OGRENCILER
     def post(self):
         parser.add_argument("ad")
         parser.add_argument("yas")
         parser.add_argument("bolum")
         args = parser.parse_args()
         
-        student_id = int(max(OGRENCİLER.keys())) + 1
+        student_id = int(max(OGRENCILER.keys())) + 1
         student_id = '%i' % student_id
-        OGRENCİLER[student_id] = {
+        OGRENCILER[student_id] = {
             "ad": args["ad"],
             "yas": args["yas"],
             "bolum": args["bolum"],
         }
-        return OGRENCİLER[student_id], 201
+        return OGRENCILER[student_id], 201
     
 class Ogrenci(Resource):
     def get(self, student_id):
-        if student_id not in OGRENCİLER:
+        if student_id not in OGRENCILER:
             return "Bulunamadı", 404
         else:
-            return OGRENCİLER[student_id]
+            return OGRENCILER[student_id]
     def put(self, student_id):
         parser.add_argument("ad")
         parser.add_argument("yas")
         parser.add_argument("bolum")
         args = parser.parse_args()
-        if student_id not in OGRENCİLER:
+        if student_id not in OGRENCILER:
             return "Kayıt bulunamadı", 404
         else:
-            ogrenci = OGRENCİLER[student_id]
+            ogrenci = OGRENCILER[student_id]
             ogrenci["ad"] = args["ad"] if args["ad"] is not None else ogrenci["ad"]
             ogrenci["yas"] = args["yas"] if args["yas"] is not None else ogrenci["yas"]
             ogrenci["bolum"] = args["bolum"] if args["bolum"] is not None else ogrenci["bolum"]
             return ogrenci, 200
 
     def delete(self, student_id):
-        if student_id not in OGRENCİLER:
+        if student_id not in OGRENCILER:
             return "Bulunamadı", 404
         else:
-            del OGRENCİLER[student_id]
+            del OGRENCILER[student_id]
             return '', 204
 
 
